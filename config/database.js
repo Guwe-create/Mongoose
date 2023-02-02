2b05d19425e7ea9d9c16d3bd2a112a435947efe1
Liam <mangoguwe@gmail.com>
2023-02-02 14:23:46 -0500

    //

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI:"mongodb+srv:Mango12345:30189He11o?!@cluster0.w21atvv.mongodb.net/?retryWrites=true&w=majority"}
+    module.exports = {mongoURI:"mongodb+srv://Mango12345:30189He11o?!@cluster0.w21atvv.mongodb.net/?retryWrites=true&w=majority"}
 }else
 {
     module.exports = {mongoURI:"mongodb://localhost:27017/gameEntries"}
