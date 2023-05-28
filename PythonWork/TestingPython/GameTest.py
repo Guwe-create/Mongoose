f25ba86fc9d99dcf346854c4286833b2f0fc369f
Guwe-create <whroy@email.neit.edu>
2023-05-27 23:31:50 -0400

    Creation of new assets and gamestates

--- PythonWork/TestingPython/GameTest.py
+++ PythonWork/TestingPython/GameTest.py
@@ -14,8 +14,11 @@ font = pygame.font.SysFont('Comic Sans MS', 30)
 text = font.render('Score: 0', False, (0,0,0))
 score = 0
 
+#Spawn Amount
+#SpawnSpeed = font.render('Speed: 0', False, (0,0,0))
+
 #Gamestates
-GameStates = 0
+GameStates = 1
 
 #Player Health
 Health = [pygame.display.set_mode((100,100)),pygame.display.set_mode((100,100)),pygame.display.set_mode((100,100))]
@@ -25,7 +28,7 @@ healthImage = pygame.image.load('Crystal.png')
 healthImage = pygame.transform.scale(healthImage,(100,100))
 
 #Player information
-pl = pygame.display.set_mode((50,50))
+pl = pygame.display.set_mode((100,100))
 player = pygame.image.load('f1.png.png')
 x = 50
 y = 50
@@ -39,35 +42,76 @@ enemyImage = pygame.transform.scale(enemyImage,(50,50))
 xPos = [770,770,770,770]
 enemySpeed = [.2,.1,.5,.15]
 enemyAmount = 4
+enemySpawnSpeed = 0
+spawnTime = 600
+
+#Making the buttons
+startButton = pygame.display.set_mode((300,200))
+exitButton = pygame.display.set_mode((300,200))
+start = pygame.image.load('StartButton.png')
+exit = pygame.image.load('EscapeButton.png')
 
 #Making the background screen
-screen = pygame.display.set_caption("Liam Game")
+screen = pygame.display.set_caption("Slime Dodge")
+backgroundImage = pygame.image.load('Background.png')
+backgroundImage = pygame.transform.scale(backgroundImage,(800,600))
 screen = pygame.display.set_mode((800,600))
 color = (0,0,200)
 
+
+def DrawMainMenu(gm):
+    screen.blit(backgroundImage,(0,0))
+    startButton.blit(start,(200,100))
+    exitButton.blit(exit,(200,300))
+    if keyboard[pygame.K_SPACE]:
+        gm = 0
+    if keyboard[pygame.K_ESCAPE]:
+        event.type = pygame.QUIT
+    pygame.display.update()
+    return gm
+
+
 #The draw function
 def Draw(xs,ys):
+    screen.blit(backgroundImage,(0,0))
     #Drawing the player and other objects
     pl.blit(player,(xs,ys))
-
-    #Drawing players health
-    for i in range(0,healthNumber):
-        Health[i].blit(healthImage,(healthX[i],-20))
-
-    #Drawing to screen
-    text = font.render('Score: {}'.format(score), False, (0,0,0))
-    screen.blit(text,(0,0))
 
     #Drawing the enemys
     for i in range(0,enemyAmount):
         enemy.blit(enemyImage,(xPos[i],yPos[i]))
 
+    #Drawing to screen
+    text = font.render('Score: {}'.format(score), False, (0,0,0))
+    screen.blit(text,(0,0))
+
+    #Drawing players health
+    for i in range(0,healthNumber):
+        Health[i].blit(healthImage,(healthX[i],-20))
+
     pygame.display.update()
     return xs, ys
 
+# Enemy Spawn Speed
+def SpawnEnemy(speed,eAmount,x,y,s,sT):
+    x = xPos
+    y = yPos
+    s = enemySpeed
+    #Respawning the enemy
+    speed += .1
+    if speed > sT:
+        eAmount += 1
+        x.append(770) 
+        y.append(random.randint(1,570))
+        s.append(random.uniform(.1,.7))
+        sT += 600
+        speed = 0
+
+    return speed,eAmount,x,y,s,sT
+
+
 # the player controls
 def ControlsAndCollision(xs,ys,xp,yp,sc,hNum):
-
      #The controls
     if keyboard[pygame.K_d]:
         xs += playerSpeed
@@ -112,7 +156,6 @@ def ControlsAndCollision(xs,ys,xp,yp,sc,hNum):
     if hNum <= 0:
         event.type = pygame.QUIT
 
-
     return xs , ys , xp , yp , sc , hNum
 
 # the gameloop
@@ -128,15 +171,20 @@ while True:
 
         #The end game sequence
         if event.type == pygame.QUIT:
-
             pygame.quit()
             quit()
 
-    #Gamestates added
+    #Drawing the update sequence
     if GameStates == 0:
         #Running the program
+        enemySpawnSpeed,enemyAmount,xPos,yPos,enemySpeed,spawnTime = SpawnEnemy(enemySpawnSpeed,enemyAmount,xPos,yPos,enemySpeed,spawnTime)
         x,y,xPos,yPos,score,healthNumber = ControlsAndCollision(x,y,xPos,yPos,score,healthNumber)
         x,y = Draw(x,y)
+
+    #Drawing the main menu
+    if GameStates == 1:
+        GameStates = DrawMainMenu(GameStates)
+
     
 
     
